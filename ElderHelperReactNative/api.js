a9464bf9cd5f97e0257070ba8e5ed345fc4e60f8
Stanley <27.stanleylch@gmail.com>
2023-11-10 08:56:45 +0000

    axios

--- ElderHelperReactNative/api.js
+++ ElderHelperReactNative/api.js
@@ -1,1 +1,16 @@
 //util functions interacting with api go here
+
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: "https://elderhelper.onrender.com/api",
+});
+
+export const fetchElderJobs = async () => {
+  return await request.get(`/jobs/elder/4`).then(({ data }) => {
+    console.log(data);
+    return data;
+  });
+};
+
+fetchElderJobs();
