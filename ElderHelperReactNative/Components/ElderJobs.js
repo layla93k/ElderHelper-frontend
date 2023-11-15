33ffb5236ecbe84e9b071bb1dcac067c0ead6b67
Yve Volkovaya <yvevolkovaya@gmail.com>
2023-11-15 17:00:32 +0000

    added elderjobs page

--- ElderHelperReactNative/Components/ElderJobs.js
+++ ElderHelperReactNative/Components/ElderJobs.js
@@ -1,93 +1,176 @@
-import { getJobsByElder } from "../api";
+import React from 'react';
+import { Alert, Text, View, StyleSheet, FlatList, Image, Button, Pressable } from 'react-native';
+import { useContext, useState, useEffect } from 'react';
+import { getJobsByElderId } from '../api';
 import { CurrentUser } from "../UserContext";
-import { useContext, useState, useEffect } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { deleteJob } from '../api';
 const moment = require("moment");
 
-export default function ElderJobs() {
-  const [elderJobsList, setElderJobsList] = useState([]);
-  const { userId } = useContext(CurrentUser);
-  const actualUserId = userId.user_id;
+export default function ElderJobList({navigation }) {
+  const { userId } = useContext(CurrentUser)
+    const [jobs, setJobs] = useState([])
+    
+    useEffect(() => {
+        getJobsByElderId(userId.user_id).then((jobs) => {
+            setJobs(jobs)
+        })
+    })
 
-  useEffect(() => {
-    getJobsByElder(actualUserId)
-      .then((response) => {
-        setElderJobsList(response);
+    const pressHandler = (job) => {navigation.navigate("SingleJob", {
+        jobData: {job}
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  console.log(elderJobsList);
+    }
 
-  const statusMap = {
-    1: "Requested",
-    2: "Accepted",
-    3: "Completed",
-    4: "Expired",
-  };
+    const deleteOwnJobHandler = (job_id) => {
+        deleteJob(job_id);
+        Alert.alert("Job deleted."); //for deleting jobs as elder
+      };
 
-  return (
-    <ScrollView vertical>
-      <View style={styles.container}>
-        <Text style={styles.text}>Your requested jobs</Text>
-        {elderJobsList.map((job) => (
-          <View style={styles.card} key={job.job_id}>
-            <Text style={styles.title}>{job.job_title}</Text>
-            <Text style={styles.description}>{job.job_desc}</Text>
-            <Text style={styles.expiration}>
-              Expires:{" "}
-              {moment(job.expiry_date.slice(0, 10)).endOf("day").fromNow()}
-            </Text>
-            <Text style={styles.status}>
-              {" "}
-              Status: {statusMap[job.status_id]}
-            </Text>
-          </View>
-        ))}
-      </View>
-    </ScrollView>
-  );
+    return (
+        <View style={styles.jobsListContainer}>
+            <View style={styles.jobsContainer}>
+                <FlatList
+                    style={styles.jobsList}
+                    keyExtractor={(item) => item.job_id }
+                    data={jobs}
+                    renderItem={({ item }) => (
+                        <View style={styles.job} onPress={() => pressHandler(item)}>
+                            <Text style={styles.jobTitle}>{item.job_title}</Text>
+                            <View style={styles.row}>
+                                <Image style={styles.jobImg}src={(userId.avatar_url)}/>
+                                <View style={styles.jobInfo}>
+                                    <View style={styles.jobUserInfo}>
+                                        <Text style={styles.jobTextUserInfo}>{item.first_name}</Text>
+                                        </View>
+                                        <Text style={styles.jobText}>Posted: {new Date (item.posted_date).toLocaleDateString()}</Text>
+                                        {item.status_id === 1 && (
+                                        <Text style={styles.jobText}>
+                                        Deadline: {new Date (item.expiry_date).toLocaleDateString()}
+                                        </Text>
+                                         )}
+                                        {item.status_id === 1 && (
+                                        <Text style={styles.jobText}>
+                                        Status: Posted
+                                        </Text>
+                                         )}
+                                         {item.status_id === 2 && (
+                                        <Text style={styles.jobText}>
+                                        Status: Taken
+                                        </Text>
+                                         )}
+                                         {item.status_id === 3 && (
+                                        <Text style={styles.jobText}>
+                                        Status: Completed
+                                        </Text>
+                                         )}
+                                         {item.status_id === 4 && (
+                                        <Text style={styles.jobText}>
+                                        Status: Expired
+                                        </Text>
+                                         )}
+                                         
+                                </View>
+                            </View>
+                            <View style = {styles.buttonView}>
+                                          <View style = {styles.editButton}>
+                                        <Button title = 'Edit'></Button></View>
+                                        <View style = {styles.editButton}><Button title = 'Delete'></Button></View>
+                                        </View>
+                        </View>
+                    )}
+                />
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#D6EAEE",
-  },
-  expiration: {
-    fontSize: 20,
-    marginLeft: 10,
-    color: "red",
-  },
-  status: {
-    fontSize: 20,
-    marginLeft: 4,
-    marginTop: 5,
-  },
-  text: {
-    fontSize: 30,
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: "bold",
-  },
-  description: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  card: {
-    shadowOffset: {
-      width: 3,
-      height: 5,
+    jobsListContainer: {
+      flex: 1,
+      backgroundColor: '#ede7d7',
+      padding: 8,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
     },
-    marginTop: 10,
-    height: 200,
-    backgroundColor: "#D6EAEE",
-    borderColor: "#0072BB",
-    borderWidth: 5,
-    borderStyle: "solid",
-    borderRadius: 20,
-    padding: 5,
-  },
-});
+    buttonContainer: {
+        flex: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonView: {
+     flex: 1 
+    },
+    jobsContainer: {
+        flex: 4,
+        marginTop: 10,
+        alignSelf: 'stretch',
+        borderColor: 'black',
+    },
+    button: {
+        backgroundColor: '#08495d',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 5,
+        elevation: 3,
+        marginTop: 10,
+      },
+      buttonText: {
+        color: '#ede7d7',
+        fontWeight: 'bold',
+        fontSize: 16,
+        letterSpacing: 0.25,
+        lineHeight: 21,
+      },
+    job: {
+        backgroundColor: '#b3e3e3',
+        borderRadius: 15,
+        flex: 1,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 18,
+        paddingRight: 18,
+        marginLeft: 12,
+        marginRight: 12,
+        marginTop: 0,
+        marginBottom: 15,
+        height: 'auto',
+        elevation: 5,
+    },
+    jobTitle: {
+        color: '#08495d',
+        alignSelf: 'center',
+        marginBottom: 10,
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    jobInfo: {
+        justifyContent: 'space-evenly',
+        alignContent: 'center',
+    },
+    jobUserInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 10,
+    },
+    jobImg: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+    },
+    jobTextUserInfo: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#08495d',
+        fontSize: 16,
+    },
+    jobText: {
+        textAlign: 'center',
+        color: '#08495d',
+    }
+  });
