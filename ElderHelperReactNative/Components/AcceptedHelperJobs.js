29dc1ff34c9991fcdaa63cd69b3a470fa95592c3
emmajohnston93 <emmajohnston93@outlook.com>
2023-11-15 16:10:29 +0000

    singlejobstlying

--- ElderHelperReactNative/Components/AcceptedHelperJobs.js
+++ ElderHelperReactNative/Components/AcceptedHelperJobs.js
@@ -30,17 +30,17 @@ export default function ElderJobs() {
     <ScrollView vertical>
       <View style={styles.container}>
         <SafeAreaView>
-          <Text style={styles.text}>Your Accepted jobs</Text>
+          <Text style={styles.text}>Your accepted jobs</Text>
           {jobsList.map((job, index) => (
             <View index={job.job_id} style={styles.card}>
-              <Text style={styles.title}>{job.job_title}</Text>
-              <Text style={styles.description}>{job.job_desc}</Text>
-              <Text>
+              <Text style={styles.jobTitle}>{job.job_title}</Text>
+              <Text style={styles.jobInfo}>{job.job_desc}</Text>
+              <Text style={styles.jobTextExp}>
                 {" "}
                 Expires:{" "}
                 {moment(job.expiry_date.slice(0, 10)).endOf("day").fromNow()}
               </Text>
-              <Text> Status: {statusMap[job.status_id]}</Text>
+              <Text style={styles.acceptedStatus}> Status: {statusMap[job.status_id]}</Text>
             </View>
           ))}
         </SafeAreaView>
@@ -51,31 +51,70 @@ export default function ElderJobs() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#D6EAEE",
-  },
+      flex: 1,
+      backgroundColor: "#ede7d7",
+      padding: 8,
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
   text: {
-    fontSize: 30,
+    fontSize: 40,
+    color: "#08495d",
+    marginBottom: 15,
+    textAlign: "center",
+  
   },
   title: {
     fontSize: 35,
     fontWeight: "bold",
   },
-  description: {
+ 
+  card: {
+    backgroundColor: "#b3e3e3",
+    borderRadius: 15,
+    // borderWidth: 1,
+    borderColor: "#08495d",
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 18,
+    paddingRight: 18,
+    marginLeft: 12,
+    marginRight: 12,
+    marginTop: 0,
+    marginBottom: 15,
+    height: "auto",
+    width: 350,
+    elevation: 5,
+  },
+
+
+  jobTitle: {
+    color: "#08495d",
+    alignSelf: "center",
+    marginBottom: 10,
+    fontWeight: "bold",
     fontSize: 20,
+  },
+  jobInfo: {
+    justifyContent: "space-evenly",
+    alignContent: "center",
+    fontSize: 17,
+    textAlign: "center",
     marginBottom: 10,
   },
-  card: {
-    shadowOffset: {
-      width: 3,
-      height: 5,
-    },
-    marginTop: 10,
-    height: 150,
-    backgroundColor: "#D6EAEE",
-    borderColor: "#0072BB",
-    borderWidth: 3,
-    borderStyle: "solid",
-    borderRadius: 20,
+  jobTextExp: {
+    textAlign: "center",
+    color: "#08495d",
+    fontSize: 15,
+    marginBottom: 5,
+  },
+  acceptedStatus: {
+    color: "green",
+    textAlign: "center",
+    fontSize: 20,
+    marginBottom: 5,
+    fontWeight: "bold",
   },
 });
