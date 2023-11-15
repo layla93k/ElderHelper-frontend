b20bac547ba23288fb4905a89c1771f33f3a3191
Layla <layla.kawafi@hotmail.com>
2023-11-15 11:17:43 +0000

    edits made to date formatting, status formatting and some styling

--- ElderHelperReactNative/Components/Login.js
+++ ElderHelperReactNative/Components/Login.js
@@ -206,7 +206,7 @@ const style = StyleSheet.create({
     backgroundColor: "#0072BB",
     width: 180,
     height: 60,
-    flexDirection: "row",
+    flex: "row",
     alignSelf: "center",
   },
   text: {
