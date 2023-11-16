86d80a1fb2c38bc56abaa4c492bcfc4807c569c8
Jakub Wiszowaty <jakubwiszowaty@hotmail.com>
2023-11-16 10:59:50 +0100

    debugging-added- stanley code, brought back previous map and job card"

--- ElderHelperReactNative/Components/JobCard.js
+++ ElderHelperReactNative/Components/JobCard.js
@@ -17,55 +17,51 @@ export default function JobCard({ press, setPress, navigation }) {
       <ScrollView
         horizontal
         snapToInterval={140}
-        style={styles.scrollView}
+        style={styles.cardContainer}
         scrollEventThrottle={1}
-        contentContainerStyle={styles.container}
-        onContentSizeChange={(contentWidth, contentHeight) => {
-          console.log('Content Size', contentWidth, contentHeight);
-        }}
-        onLayout={(event) => {
-          console.log('Layout Size', event.nativeEvent.layout);
-        }}
+        contentContainerStyle={styles.endPadding}
       >
-        {!press ? (
-          <Text style={styles.noJobsText}>Choose an area</Text>
-        ) : jobs.length === 0 ? (
-          <Text style={styles.noJobsText}> No jobs in {press}</Text>
-        ) : (
-          jobs.map((job, index) => (
-            <View key={index} style={styles.card}>
-              <Text style={styles.title}>{job.job_title}</Text>
-              <Text style={styles.description} numberOfLines={3}>
-                {job.job_desc}
-              </Text>
-              <Button
-                title="more info"
-                style={styles.button}
-                onPress={() =>
-                  navigation.navigate("SingleJob", {
-                    jobData: { job },
-                  })
-                }
-              />
-            </View>
-          ))
+        <View style={styles.container}>
+          {!press ? (
+            <Text style={styles.noJobsText}>Choose an area</Text>
+          ) : jobs.length === 0 ? (
+            <Text style={styles.noJobsText}>
+              Sorry, unfortunately there are no jobs in {press}
+            </Text>
+          ) : (
+            jobs.map((job, index) => (
+              <View key={index} style={styles.card}>
+                <Text style={styles.title}>{job.job_title}</Text>
+                <Text style={styles.description} numberOfLines={3}>
+                  {job.job_desc}
+                </Text>
+                <Button
+                  title="more info"
+                  style={styles.button}
+                  onPress={() =>
+                    navigation.navigate("SingleJob", {
+                      jobData: { job },
+                    })
+                  }
+                />
+              </View>
+            ))
           )}
+        </View>
       </ScrollView>
     </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    position: 'relative',
-  },
-  container: {
-    flexDirection: "row",
+  button: {
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
     padding: 10,
   },
   card: {
-    overflow: "hidden",
-    height: 160,
+    height: "auto",
     width: 140,
     backgroundColor: "#D6EAEE",
     borderColor: "#0072BB",
@@ -73,6 +69,9 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     borderRadius: 20,
     margin: 3,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -81,7 +80,11 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 6,
     elevation: 5,
-    },
+  },
+  container: {
+    flexDirection: "row",
+    overflow: "scroll",
+  },
   title: {
     marginTop: 18,
     fontSize: 18,
@@ -92,7 +95,25 @@ const styles = StyleSheet.create({
   description: {
     textAlign: "center",
   },
+  cardContainer: {
+    height: 200,
+    flexWrap: "nowrap",
+    bottom: 0,
+    marginBottom: 5,
+    position: "absolute",
+    flexDirection: "row",
+  },
+
+  endPadding: {
+    padding: 10,
+    paddingRight: 16,
+  },
   noJobsText: {
-    fontSize: 30,
+    fontSize: 20,
+    backgroundColor: "#D6EAEE",
+    height: 50,
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 10,
   },
 });
